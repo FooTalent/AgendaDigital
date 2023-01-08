c9cd10f9569697404f72b33f975335ec835ae5c2
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-01-07 19:45:27 -0500

    cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import adminRoutes from './routes/adminRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import cors from 'cors';
 const app = express();
+app.use(cors())
 app.use(express.json());
 dotenv.config();
 connectDB();
