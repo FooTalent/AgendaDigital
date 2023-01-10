3176e9fc3b058e2e92da4513b60c9b7768ba5a20
Leandro Marelli <leandromarelli@gmail.com>
2023-01-10 20:24:04 -0300

    primer carga dashboard

--- FrontEnd/src/pages/dashboard/Dashboard.jsx
+++ FrontEnd/src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import "./dashboard.css"
+import { NavLink } from 'react-router-dom';
 
 const Dashboard = () => {
-    return (
-        <div>
-            dashboard
+  return (
+    <div className="container">
+      <NavLink className="navDeco" to={"#"}>
+      <div className="card">
+        <div className="element">
+          <p className="cardTitle">Asignatura</p>
+          <p className="cardText">codigo**</p>
         </div>
-    );
+        <div className='btn'>
+          <button className="btnInfo">+informacion</button>
+          <img className="imgInfo" src="../../../public/img/infoIcon.png" alt="info Icon" />
+        </div>
+      </div>
+      </NavLink>
+      <NavLink className="navDeco" to={"#"}>
+      <div className="card">
+        <div className="element">
+          <p className="cardTitle">Asignatura</p>
+          <p className="cardText">codigo**</p>
+        </div>
+        <div className='btn'>
+          <button className="btnInfo">+informacion</button>
+          <img className="imgInfo" src="../../../public/img/infoIcon.png" alt="info Icon"/>
+        </div>
+      </div>
+      </NavLink>
+      <NavLink className="navDeco" to={"#"}>
+      <div className="card">
+        <div className="element">
+          <p className="cardTitle">Asignatura</p>
+          <p className="cardText">codigo**</p>
+        </div>
+        <div className='btn'>
+          <button className="btnInfo">+informacion</button>
+          <img className="imgInfo" src="../../../public/img/infoIcon.png" alt="info Icon" />
+        </div>  
+      </div>
+      </NavLink>
+    </div>
+  );
 };
 
 export default Dashboard;
