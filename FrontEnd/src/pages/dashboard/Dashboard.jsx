6899ebb72822df45b19462753608e1ecf5ca2be8
Santi1994r <santiagoruiz9416@gmail.com>
2023-01-12 22:01:03 -0300

    termino estilos del sidebar

--- FrontEnd/src/pages/dashboard/Dashboard.jsx
+++ FrontEnd/src/pages/dashboard/Dashboard.jsx
@@ -1,48 +1,62 @@
-import React from 'react';
-import "./dashboard.css"
-import { NavLink } from 'react-router-dom';
-import SideBar from '../../components/SideBar/SideBar';
+import React from "react";
+import "./dashboard.css";
+import { NavLink } from "react-router-dom";
+import SideBar from "../../components/SideBar/SideBar";
 
 const Dashboard = () => {
   return (
-    <div className="container">
+    <div className="containerDashboard">
       <SideBar />
-      <NavLink className="navDeco" to={"#"}>
-      <div className="card">
-        <div className="element">
-          <p className="cardTitle">Asignatura</p>
-          <p className="cardText">codigo*</p>
-        </div>
-        <div className='btn'>
-          <button className="btnInfo">+informacion</button>
-          <img className="imgInfo" src="../../../public/img/infoIcon.png" alt="info Icon" />
-        </div>
+      <div className="container">
+        <NavLink className="navDeco" to={"#"}>
+          <div className="card">
+            <div className="element">
+              <p className="cardTitle">Asignatura</p>
+              <p className="cardText">codigo*</p>
+            </div>
+            <div className="btn">
+              <button className="btnInfo">+informacion</button>
+              <img
+                className="imgInfo"
+                src="../../../public/img/infoIcon.png"
+                alt="info Icon"
+              />
+            </div>
+          </div>
+        </NavLink>
+        <NavLink className="navDeco" to={"#"}>
+          <div className="card">
+            <div className="element">
+              <p className="cardTitle">Asignatura</p>
+              <p className="cardText">codigo**</p>
+            </div>
+            <div className="btn">
+              <button className="btnInfo">+informacion</button>
+              <img
+                className="imgInfo"
+                src="../../../public/img/infoIcon.png"
+                alt="info Icon"
+              />
+            </div>
+          </div>
+        </NavLink>
+        <NavLink className="navDeco" to={"#"}>
+          <div className="card">
+            <div className="element">
+              <p className="cardTitle">Asignatura</p>
+              <p className="cardText">codigo**</p>
+            </div>
+            <div className="btn">
+              <button className="btnInfo">+informacion</button>
+              <img
+                className="imgInfo"
+                src="../../../public/img/infoIcon.png"
+                alt="info Icon"
+              />
+            </div>
+          </div>
+        </NavLink>
       </div>
-      </NavLink>
-      <NavLink className="navDeco" to={"#"}>
-      <div className="card">
-        <div className="element">
-          <p className="cardTitle">Asignatura</p>
-          <p className="cardText">codigo**</p>
-        </div>
-        <div className='btn'>
-          <button className="btnInfo">+informacion</button>
-          <img className="imgInfo" src="../../../public/img/infoIcon.png" alt="info Icon"/>
-        </div>
-      </div>
-      </NavLink>
-      <NavLink className="navDeco" to={"#"}>
-      <div className="card">
-        <div className="element">
-          <p className="cardTitle">Asignatura</p>
-          <p className="cardText">codigo**</p>
-        </div>
-        <div className='btn'>
-          <button className="btnInfo">+informacion</button>
-          <img className="imgInfo" src="../../../public/img/infoIcon.png" alt="info Icon" />
-        </div>  
-      </div>
-      </NavLink>
     </div>
   );
 };
