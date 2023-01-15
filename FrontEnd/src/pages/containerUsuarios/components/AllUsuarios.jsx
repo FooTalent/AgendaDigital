eb87df3a0a8a45811dbd89c0733e47d8942caab2
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-01-15 12:13:48 -0300

    Realice el modal con SweetAlert para editar usuarios

--- FrontEnd/src/pages/containerUsuarios/components/AllUsuarios.jsx
+++ FrontEnd/src/pages/containerUsuarios/components/AllUsuarios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../../Context/GlobalContext";
+import Swal from "sweetalert2";
 import "./allusuarios.css";
 
 const AllUsuarios = ({ search }) => {
@@ -24,6 +25,34 @@ const AllUsuarios = ({ search }) => {
   };
  
   searchName();
+
+  const editUsers = async (dni, nombre, email) => {
+    
+    const {value: formValues} = await Swal.fire({
+      title: 'Edit Users',
+      html:
+        '<label>DNI</label>'+
+        `<input type="text" id="swal-input1" class="swal2-input" value=${dni} >` +
+        '<label>NOMBRE </label>'+
+        `<input id="swal-input2" class="swal2-input" value=${nombre}>`+
+        '<label>EMAIL</label>'+
+        `<input type="email" id="swal-input3" class="swal2-input" value=${email}>`,
+      focusConfirm: false,
+      background: '#DAE2FE',
+      color: '#6928F3',
+      preConfirm: () => {
+        return [
+          document.getElementById('swal-input1').value,
+          document.getElementById('swal-input2').value,
+          document.getElementById('swal-input3').value
+        ]
+      }
+    })
+    if (formValues) {
+      console.log(JSON.stringify(formValues))
+    }
+   
+  }
   return (
     <div>
       <table className="allusers-datadable">
@@ -54,7 +83,7 @@ const AllUsuarios = ({ search }) => {
               <td className="allusers-tbody2">
                 <span>
                   {" "}
-                  <img src="../public/img/edit.png" alt="" />{" "}
+                  <img onClick={()=>{editUsers(us._id,us.name,us.email)}} src="../public/img/edit.png" alt="" />{" "}
                 </span>
                 <span>
                   {" "}
