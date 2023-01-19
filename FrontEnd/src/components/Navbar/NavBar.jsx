2354e473a6cc0294ac6eb55338845133210e0350
Santi1994r <santiagoruiz9416@gmail.com>
2023-01-19 14:46:57 -0300

    termino mobile del select mas chico

--- FrontEnd/src/components/Navbar/NavBar.jsx
+++ FrontEnd/src/components/Navbar/NavBar.jsx
@@ -3,13 +3,25 @@ import { GlobalContext } from "../../Context/GlobalContext";
 import "./navbar.css";
 
 const NavBar = () => {
+    let widthWindow = window.innerWidth;
   const { setEnabledSB, setEnabled, enabled, setMaximunWidth } = useContext(GlobalContext);
+ /*  const [checkWindow, setCheckWindow] = useState(widthWindow); */
 
     const handlerSideBar = () => {
         setEnabledSB(false);
         setEnabled(true);
         setMaximunWidth(false)
     };
+
+   
+    const handlerWindow = () => {
+        widthWindow
+    };
+    
+    window.addEventListener('DOMContentLoaded', handlerWindow);
+
+    
+    
 
   return (
     <div className={`containerAll ${enabled ? "disabledContainerNavbar" : null}`}>
@@ -23,7 +35,11 @@ const NavBar = () => {
         </div>
         <div className="selectUser containerSelect">
           <img src="./img/user.svg" alt="user" />
-          <select className="selectUser" name="selectUser" id="selectUser">
+          <select className="selectUser selectWidth" name="selectUser" id="selectUser">
+            {
+                widthWindow < 576 && <option value=""></option>               
+            }
+            
             <option className="optionValue" value="admin">
               Admin
             </option>
