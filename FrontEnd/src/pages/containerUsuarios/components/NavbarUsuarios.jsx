f9952b2996bd31e3c5dbb2b88700d56aba919bc0
Brajhan Lopez <bralopezsuasnabar@gmail.com>
2023-01-27 14:46:55 -0500

    arreglando imagenes 3

--- FrontEnd/src/pages/containerUsuarios/components/NavbarUsuarios.jsx
+++ FrontEnd/src/pages/containerUsuarios/components/NavbarUsuarios.jsx
@@ -5,7 +5,7 @@ const NavbarUsuarios = () => {
     return (
         <div className='usersAll-nav'>
             
-            <NavLink to='../usuarios/all' className='userAll-btn' style={({ isActive }) => isActive ? {background: '#6889FF', borderTop: '#6928F3 solid 2px'}: undefined}><img src="../public/img/allusers.png" width='24px' height='24px' alt=""  /> Todos los Usuarios</NavLink>
+            <NavLink to='../usuarios/all' className='userAll-btn' style={({ isActive }) => isActive ? {background: '#6889FF', borderTop: '#6928F3 solid 2px'}: undefined}><img src="../img/allusers.png" width='24px' height='24px' alt=""  /> Todos los Usuarios</NavLink>
             <NavLink to='../usuarios/add' className='userAll-btn' style={({ isActive }) => isActive ? {background: '#6889FF', borderTop: '#6928F3 solid 2px'}: undefined}>+ Agregar Usuario</NavLink>
                     </div>
     );
